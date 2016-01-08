c0d42bd6e0d4381e9bcbdf89901bb385cd68d265
uenokenji <salesdesignlabtv@gmail.com>
2016-01-08 15:06:35 +0000

    add sign sign up and user detail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+  
+  def show # 追加
+   @user = User.find(params[:id])
+  end
+  
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/new'
-
   root to: 'static_pages#home'
+  get 'signup',  to: 'users#new'
+
+  resources :users
 end
