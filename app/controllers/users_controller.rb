5b52d8a7cd856887d043aa681d622f1e2bf9f8c7
uenokenji <salesdesignlabtv@gmail.com>
2016-01-09 13:55:19 +0000

    kadai

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+before_action :set_user, only: [:edit, :update]
+
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -7,7 +8,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  
+  def edit # 課題
+  end
+  
+  def update # 課題
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: "update OK!"
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -21,7 +35,10 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :area, :password,
                                  :password_confirmation)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
