d9cc3fcedab207796c4a71b4f37c761aa94626af
uenokenji <salesdesignlabtv@gmail.com>
2016-01-12 09:40:33 +0000

    kadai0112

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112090448) do
+ActiveRecord::Schema.define(version: 20160112093619) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "area"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160112090448) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "area"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
