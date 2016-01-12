2d0bf1407391dc74091e9dcaf5cf4007349d224c
uenokenji <salesdesignlabtv@gmail.com>
2016-01-12 09:13:09 +0000

    micropost_kadai

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108125611) do
+ActiveRecord::Schema.define(version: 20160112090448) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "area"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
