de3443fe55a1c3579d1e7e0560c5a5db016846dc
uenokenji <salesdesignlabtv@gmail.com>
2016-01-19 11:53:28 +0000

    follower0119

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112111826) do
+ActiveRecord::Schema.define(version: 20160118041707) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "favorites", ["created_at"], name: "index_favorites_on_created_at"
+  add_index "favorites", ["tweet_id"], name: "index_favorites_on_tweet_id"
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
 
   create_table "messages", force: :cascade do |t|
     t.string   "area"
